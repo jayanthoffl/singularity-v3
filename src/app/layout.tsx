dcdde0a6176628ef0afd1de7d430fd13e867bba7
Jayanth Ramakrishnan <jayanthvasudeva@gmail.com>
2026-03-19 18:22:39 +0530

    Changed the name of website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ const unbounded = Unbounded({
 });
 
 export const metadata = {
-  title: 'Singularity | Student Research Lab',
-  description: 'The Singularity Advanced Research Lab at SRMAP.',
+  title: 'Singularity Student Lab | SRMAP',
+  description: 'The Singularity Student Lab at SRM University AP ( SRMAP ) is a cutting-edge research hub where students explore the frontiers of technology and innovation. The lab focuses on AI/ML, Quantum Computing, and more, fostering creativity and collaboration to shape the future of science and technology. It is SRMAP second Student-Led Initiative',
 };
 
 export default function RootLayout({
